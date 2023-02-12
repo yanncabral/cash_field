50413da27419d8db325b824d09c6d21a0e9484ae
Yann <iamyanndias@gmail.com>
2023-02-12 19:40:42 -0300

    Add initial dart template

---
+++ example/cash_field_example.dart
@@ -0,0 +1,6 @@
+import 'package:cash_field/cash_field.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/cash_field_test.dart
@@ -0,0 +1,16 @@
+import 'package:cash_field/cash_field.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
