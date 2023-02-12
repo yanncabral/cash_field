3bcbeca32898759a24c9c6ed19c446be1276c5d4
Yann <iamyanndias@gmail.com>
2023-02-12 19:59:28 -0300

    Add CashCubit with business logic to the package.

---
+++ lib/src/cubit/cash_cubit.dart
@@ -0,0 +1,28 @@
+import 'package:bloc/bloc.dart';
+import 'package:cash_field/src/cubit/cash_state.dart';
+
+///
+class CashCubit extends Cubit<CashState> {
+  /// Create a new [CashCubit] with an optional [initialValue] and
+  /// [decimalDigits].
+  CashCubit({
+    int initialValue = 0,
+    int decimalDigits = 2,
+  }) : super(CashState(value: initialValue, decimalDigits: decimalDigits));
+
+  /// Add a digit to the end of the current value.
+  void addDigit(int digit) {
+    if (digit > 9 || digit < 0) {
+      throw ArgumentError('Number must be between 0 and 9.');
+    }
+
+    final newValue = state.value * 10 + digit;
+    emit(state.copyWith(value: newValue));
+  }
+
+  /// Remove the last digit from the current value.
+  void removeDigit() {
+    final newValue = state.value ~/ 10;
+    emit(state.copyWith(value: newValue));
+  }
+}
